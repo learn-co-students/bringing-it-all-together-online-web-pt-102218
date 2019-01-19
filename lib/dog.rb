390795ffa0a4403646d4e1a45ebda1ebf458d4be
grant smith <grantsmith1256@gmail.com>
2019-01-19 04:03:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,7 +26,7 @@ class Dog
   end
 
   def self.create(name:, breed:)
-    instance = self.new(name: "#{name}", breed: "#{breed}")
+    instance = self.new(name: name, breed: breed)
     instance.save
     instance
   end
