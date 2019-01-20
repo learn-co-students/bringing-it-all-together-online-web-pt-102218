e454520b2c40eba283703b5c857ba4343fcd923c
Johnathon Wood <jaywood128@gmail.com>
2019-01-20 23:41:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,6 +6,7 @@ class Dog
 
 
   def initialize(id: nil, name:, breed:)
+    @id = id
     @name = name
     @breed = breed
   end
@@ -55,10 +56,9 @@ class Dog
 
   def update
     sql = <<-SQL
-    "UPDATE dogs SET id = ?, name = ?, breed = ? WHERE id = ?"
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
-
+     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
   def self.find_by_id(id)
@@ -67,6 +67,34 @@ class Dog
       SQL
     result = DB[:conn].execute(sql, id)[0]
     self.new(id: result[0], name: result[1], breed: result[2])
-    binding.pry
+
+  end
+
+  def self.find_or_create_by(name:, breed:)
+
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def self.new_from_db(row)
+    dog = self.new(id: row[0],name: row[1], breed:row[2])
+    dog
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+          SELECT * FROM dogs WHERE name = ?
+            SQL
+
+      result = DB[:conn].execute(sql, name)[0]
+      dog = self.new(id: result[0], name: result[1], breed: result[2])
+      dog
+
   end
 end
