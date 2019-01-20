78266788f959ded74edebe2e1c6b629528c852e8
grant smith <grantsmith1256@gmail.com>
2019-01-20 03:16:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,14 +38,43 @@ class Dog
       sql = <<-SQL
       INSERT INTO dogs (name, breed) VALUES (?, ?)
       SQL
-      DB[:conn].execute(sql, self.name, self.breed)
+      DB[:conn].execute(sql, name, breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
+    self
   end
 
-  def self.update
+  def self.find_by_name(name)
+    sql = <<-SQL
+     SELECT * FROM dogs WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[1], dog_data[2], dog_data[0])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
+  def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.id, self.name, self.breed)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
   def self.new_from_db(row)
